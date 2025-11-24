f912d48c1ff3b2118bafff984a3e9ddadbd19e14
Новый юзер <steam.forumfelp@gmail.com>
2025-11-24 19:27:07 +0200

    333

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import localFont from "next/font/local";
+
+// Настройка кастомного шрифта Quincy CF
+const quincy = localFont({
+  src: [
+    {
+      path: './fonts/Fontspring-DEMO-quincycf-regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Fontspring-DEMO-quincycf-bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-quincy',
+  display: 'swap',
+});
 
 export const metadata = {
   title: "CalmCraft",
@@ -18,10 +37,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-        <link href="https://fonts.cdnfonts.com/css/quincy-cf" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" async></script>
       </head>
-      <body>
+      <body className={quincy.variable}>
         <Navbar />
         <main className="min-vh-100">
           {children}
