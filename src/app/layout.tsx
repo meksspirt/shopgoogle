78ae2f2870bd456c0f9f64adcd07f44cc76def86
Новый юзер <steam.forumfelp@gmail.com>
2025-11-28 17:12:33 +0200

    chore(layout): Update Google Analytics tracking ID
    - Replace Google Analytics measurement ID from G-3KPG0NGNYB to G-RTC49E8P33
    - Update gtag script source URL with new tracking ID
    - Update gtag configuration call with new tracking ID
    - Ensures analytics data is sent to correct Google Analytics property

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,14 +44,14 @@ export default function RootLayout({
     <html lang="uk">
       <head>
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3KPG0NGNYB"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RTC49E8P33"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-3KPG0NGNYB');
+              gtag('config', 'G-RTC49E8P33');
             `,
           }}
         />
